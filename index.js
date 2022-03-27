a30827e2e53c98a63c1e54bd02e164fb362988eb
Daniel Guzman <guzdan@live.com>
2022-03-27 13:57:55 -0500

    commit tree e6243622b87e580ea28c10a95bba9b8c443b5a2b

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+//Remove element with 'main' id
+const main = document.getElementById('main');
+main.remove()
+
+//Create a new h1
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'YOUR-NAME is the champion'
+document.body.append(newHeader)
